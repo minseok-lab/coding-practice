95089bd0824c8fb208aa4eed094cf1c2d9253c9b
김민석 <rlaalstjr0806@gmail.com>
2025-09-20 14:09:22 +0900

    [level 0] Title: 특수문자 출력하기, Time: 152.61 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public static void main(String[] args) {
+        // 입력값을 출력합니다.
+        // '와 "를 출력하기 위해 \를 추가 삽입합니다.
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
