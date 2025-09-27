8de5e08b981078aa50275a191c4e2e180247beb5
김민석 <rlaalstjr0806@gmail.com>
2025-09-27 12:35:30 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        //myString을 toUpperCase()로 대문자로 변환
+        String answer = myString.toUpperCase();
+        return answer;
+    }
+}
