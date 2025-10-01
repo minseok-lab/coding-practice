3452a9f868eacbb51a282156d58dc1921367cbfc
김민석 <rlaalstjr0806@gmail.com>
2025-10-01 19:00:36 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.05 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (num1 == num2) { //만약 num1과 num2가 같다면
+            return answer = 1; //1 출력
+        }
+        else return answer = -1; //그 외의 경우, -1 출력
+    }
+}
