cc962b264f2c77b3d9f173d083a9e98c856bb22b
김민석 <rlaalstjr0806@gmail.com>
2025-10-01 18:59:58 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int num1, int num2) { //num1, num2 정의
+        int answer = num1 / num2; // 정답 = num1 나누기 num2
+        return answer; // answer return
+    }
+}
