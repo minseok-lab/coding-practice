2d68a12c9d0b17920ae8b268cf39a197f271c3c4
김민석 <rlaalstjr0806@gmail.com>
2025-10-01 19:00:10 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int num1, int num2) { //int(정수) num1, num2 정의
+        int result = num1 % num2; //num1을 num2로 나눈 나머지, 나머지 연산자 % 활용
+        return result; //결과값 산출
+    }
+}
