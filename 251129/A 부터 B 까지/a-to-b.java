6fc24b75c08e082d42ba9ebd3ab1c87026ea55d9
김민석 <rlaalstjr0806@gmail.com>
2025-11-29 23:56:19 +0900

    Title: A 부터 B 까지, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251129/A 부터 B 까지/a-to-b.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int num = a;
+
+        while (num <= b) {
+            System.out.print(num + " ");
+            if (num % 2 !=0) {
+                num *= 2;
+            } else {
+                num += 3;
+            }
+        }
+    }
+}
