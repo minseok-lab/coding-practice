de3ffba399aac07be9c03f55394e1da61ef3c268
김민석 <rlaalstjr0806@gmail.com>
2025-12-09 20:58:34 +0900

    Title: 데이터 교환, Time: 81ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251209/데이터 교환/exchange-data.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp1 = a;
+        int temp2 = b;
+
+        a = c;
+        b = temp1;
+        c = temp2;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
